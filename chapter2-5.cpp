a1bb73f14a25a6883e8eecbabb19228c92cc74fe
Yinsicheng Jiang <s2020153@ed.ac.uk>
2023-11-24 00:40:53 +0000

    Update chapter2-5.cpp

--- chapter2-5.cpp
+++ chapter2-5.cpp
@@ -1,11 +1,10 @@
-#include <torch/extension.h>
-
 //custom_add.cpp
+#include <torch/extension.h>
 
 torch::Tensor custom_add(torch::Tensor a, torch::Tensor b) {
     return a + b;
 }
 
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+PYBIND11_MODULE("custom_extensions", m) {
     m.def("custom_add", &custom_add, "A custom add function");
 }
